f866ff09c2015103f5a14d3356116094b01ec9ae
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-02-26 10:57:13 +0000

    Implement GetCurExecutablePath for BSD platforms
    FreeBSD and other other BSD platforms do not provide procfs as standard;
    and if if mounted the paths to retrieve the current executable path
    differ. On BSD platforms the currently executable path can be retreived
    through issuing a sysctl.

--- src/base/utils.cc
+++ src/base/utils.cc
@@ -33,6 +33,10 @@
 #include <unistd.h>  // For getpagesize() and geteuid() & fork()
 #endif
 
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_BSD)
+#include <sys/sysctl.h>
+#endif
+
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_APPLE)
 #include <mach-o/dyld.h>
 #include <mach/vm_page_size.h>
@@ -253,13 +257,20 @@ void Daemonize(std::function<int()> parent_cb) {
 
 std::string GetCurExecutablePath() {
   std::string self_path;
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) ||   \
+#if (PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) ||   \
     PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) || \
-    PERFETTO_BUILDFLAG(PERFETTO_OS_FUCHSIA)
+    PERFETTO_BUILDFLAG(PERFETTO_OS_FUCHSIA)) && !PERFETTO_BUILDFLAG(PERFETTO_OS_BSD)
   char buf[PATH_MAX];
   ssize_t size = readlink("/proc/self/exe", buf, sizeof(buf));
   PERFETTO_CHECK(size != -1);
   // readlink does not null terminate.
+  self_path = std::string(buf, static_cast<size_t>(size));
+#elif PERFETTO_BUILDFLAG(PERFETTO_OS_BSD)
+  char buf[PATH_MAX];
+  int oid[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
+  size_t size = sizeof(buf);
+  int rc = sysctl(oid, 4, buf, &size, NULL, 0);
+  PERFETTO_CHECK(rc != -1);
   self_path = std::string(buf, static_cast<size_t>(size));
 #elif PERFETTO_BUILDFLAG(PERFETTO_OS_APPLE)
   uint32_t size = 0;
